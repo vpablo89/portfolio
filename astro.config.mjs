0585a02fffd36eb042eec6837886c09e3bedc747
Pablo Vera <verapablodaniel.1989@gmail.com>
2024-11-29 07:31:20 -0300

    fixed some bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
+import icon from "astro-icon"
 
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
+  integrations: [tailwind(), robotsTxt(), icon()],
   site: 'https://porfolio.dev/'
 })
