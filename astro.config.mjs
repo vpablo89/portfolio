3a0cc2fa3dcdefcf5f5663fe33e3c273dda46830
Pablo Vera <verapablodaniel.1989@gmail.com>
2024-11-29 16:44:07 -0300

    fixed bug related with icon dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
-import icon from "astro-icon"
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import robotsTxt from "astro-robots-txt";
 
-import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt(), icon()],
   site: 'https://porfolio.dev/'
-})
+});
